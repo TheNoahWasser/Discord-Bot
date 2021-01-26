610eb8a07623adf5c0f9b48c6925d557d857b0f5
Noah Wasser <online@noahwasser.de>
2021-01-26 02:46:54 +0100

    fetch cleanup

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
+import os
+
+logBarrier = "-----------------------------------------------------"
+
 def fetchKey():
+    print(logBarrier)
+    print("Getting API-Key...")
     with open("api_key", "r") as file:
+        print("Got Key. Key is: " + file.read())
+        print(logBarrier)
         return file.read()
+
+def fetchAccounts():
+    print(logBarrier)
+    print("Fetching Accounts...")
+    with os.scandir() as it:
+        for entry in it:
+            if not entry.name.startswith('.') and entry.is_file() and entry.name.endswith(".py"):
+                print(entry.name)
+    print("Fetching Accounts done")
+    print(logBarrier)
+    return
 
 def main():
     apikey = fetchKey()
-    print("ooga")
-    print(apikey)
-    print("booga")
+    fetchAccounts()
 
 if __name__ == "__main__":
     main()
